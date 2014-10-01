9eb0d1aabc40e4bc546003c89338198c2200e1f5
Shane Kelly <shanekweb@icloud.com>
2014-10-01 14:26:40 +0100

    pushed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Super::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_controller.asset_host = "http://superbots.s3.amazonaws.com"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
