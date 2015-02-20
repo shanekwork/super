f4a1cc9a0e0fd5bbc72e68e87380c646374f617e
Shane Kelly <shanekweb@icloud.com>
2015-02-20 16:35:05 +0000

    store logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -11,7 +11,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   config.site_name = "Office Site"
-  config.logo = "store/yourlogo.png"
+  config.logo = "store/logo.png"
   config.track_inventory_levels = false
   allow_backorder_shipping = true
   config.company = true
