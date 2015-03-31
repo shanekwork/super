69d35a531b52518b635aa86b19a7ec553cf1cd59
beaic3 <beaic3>
2015-03-31 14:09:15 +0100

    Jquery dropdown main menu

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -55,10 +55,7 @@ $(function(){
     
     });
     
-    // Add down arrow only to menu items with submenus
-    $("#main-nav-bar > li:has('ul')").each(function() {
-        $(this).find("a:first").css("background", "transparent url('/assets/down-menu-arrow.png') no-repeat right 0.35em");
-    });
+    
     
     
 });
